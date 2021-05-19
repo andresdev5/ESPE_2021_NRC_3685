65afb7f46ee47e837431d5e1c86356dfa96b6ace
Andres Jonathan <whitedev93@gmail.com>
2021-05-19 15:31:22 -0500

    updated unit test for templates workshop

--- workshops/19052021_templates/tests/test_math_utils.cpp
+++ workshops/19052021_templates/tests/test_math_utils.cpp
@@ -1,10 +1,56 @@
 #include "../math_utils.h"
 
-void test_integers() {
-    int result = MathUtils<int>::add(2, 8);
-    static_assert(std::is_same<decltype(result), int>::value, "expected int type");
+void check_return_type() {
+    if (!std::is_same<decltype(MathUtils<int>::add(2, 8)), int>::value) {
+        throw std::runtime_error("expected int type");
+    }
+}
+
+void check_integer() {
+    int result = MathUtils<int>::add(10, 5);
+
+    if (result != 15) {
+        throw std::runtime_error("check_integer(): invalid add() result");
+    }
+
+    result = MathUtils<int>::subtract(5, 4);
+
+    if (result != 1) {
+        throw std::runtime_error("check_integer(): invalid subtract() result");
+    }
+
+    result = MathUtils<int>::multiply(2, 5);
+
+    if (result != 10) {
+        throw std::runtime_error("check_integer(): invalid multiply() result");
+    }
+
+    result = MathUtils<int>::divide(8, 2);
+
+    if (result != 4) {
+        throw std::runtime_error("check_integer(): invalid divide() result");
+    }
+}
+
+void check_float() {
+    float result = MathUtils<float>::add(2.5, 2.5);
+
+    if (result != 5.f) {
+        throw std::runtime_error("check_float(): invalid add() result");
+    }
+}
+
+void check_double() {
+    double result = MathUtils<double>::add(9.4444, 2.3333333333333);
+
+    if (result != (9.4444 + 2.3333333333333)) {
+        throw std::runtime_error("check_double(): invalid add() result");
+    }
 }
 
 int main() {
-    test_integers();
+    check_return_type();
+    check_integer();
+    check_float();
+    check_double();
 }
