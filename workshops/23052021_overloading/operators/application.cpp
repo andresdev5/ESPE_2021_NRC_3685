37835f20ba148fb11b1c6ebd5531b360c036d59e
Andres Jonathan <whitedev93@gmail.com>
2021-05-25 20:55:33 -0500

    added functions project base

--- workshops/23052021_overloading/operators/application.cpp
+++ workshops/23052021_overloading/operators/application.cpp
@@ -19,18 +19,21 @@ void Application::run() {
         std::cout << "Sobrecarga de operadores ejemplos" << std::endl << std::endl;
         std::cout << "1: matrices (+, -, ~)" << std::endl;
         std::cout << "2: usuarios (==, !=, <, >)" << std::endl;
-        std::cout << "3: salir" << std::endl;
+        std::cout << "3: -" << std::endl;
+        std::cout << "4: -" << std::endl;
+        std::cout << "5: -" << std::endl;
+        std::cout << "6: salir" << std::endl;
         std::cout << std::endl;
 
         do {
             std::cout << "ingresa una opcion: ";
             std::cin >> opcion;
 
-            if (opcion < 1 || opcion > 3) {
+            if (opcion < 1 || opcion > 6) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
-        } while (opcion < 1 || opcion > 3);
+        } while (opcion < 1 || opcion > 6);
 
         switch (opcion) {
             case 1: {
@@ -111,13 +114,18 @@ void Application::run() {
                     std::cout << "los 2 usuarios tienen la misma edad" << std::endl;
                 }
             }
+            case 3: {}
+            break;
+            case 4: {}
+            break;
+            case 5: {}
+            break;
             break;
         }
 
-
-        if (opcion != 3) {
+        if (opcion != 6) {
             std::cout << std::endl << std::endl;
             system("pause");
         }
-    } while (opcion != 3);
+    } while (opcion != 6);
 }
