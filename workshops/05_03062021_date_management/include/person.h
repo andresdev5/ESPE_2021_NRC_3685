f86da72f08974a1159084edab9491fca276e9888
LizzetteZapata07 <zapatalizzette@gmail.com>
2021-06-04 00:20:33 -0500

    add person class

--- workshops/05_03062021_date_management/include/person.h
+++ workshops/05_03062021_date_management/include/person.h
@@ -7,6 +7,15 @@ class Person {
 public:
     Person(std::string identiy_card, std::string name);
 
+    void set_identify_card (std::string set_identify_card);
+    std::string get_identify_card();
+    
+    void set_name (std:: string name);
+    std::string get_name ();
+
+    void set_payment_dates (std::vector <Date> payment_dates);
+    std::vector <Date> payment_dates ();
+
 private:
     std::string identify_card;
     std::string name;
