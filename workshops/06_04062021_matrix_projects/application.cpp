ae8c981c328c0e77d8ea3270cd5cfbf276529148
sdiego616 <sdjimenez4@espe.edu.ec>
2021-06-09 01:03:03 -0500

    minor changes

--- workshops/06_04062021_matrix_projects/application.cpp
+++ workshops/06_04062021_matrix_projects/application.cpp
@@ -4,16 +4,9 @@
 #include <cstdlib>
 #include "sudoku/sudoku.h"
 #include "pascal_triangle/operation.h"
-<<<<<<< HEAD
 #include "latin_square/matrix.h"
 #include "latin_square/matrix_utils.h"
 #include "latin_square/utils.h"
-=======
-#include "magic_square/CuadradoMagico.h"
-#include "identity_matrix/matrix.h"
-#include "identity_matrix/identity_matrix.h"
-#include "latin_square/latin_square.h"
->>>>>>> 4d9124bb40aa3f3314e8aede14bf9e21864a4f2c
 
 Application &Application::get_instance() {
     static auto &&instance = Application();
@@ -27,15 +20,9 @@ void Application::run() {
         system("cls");
         std::cout << "1: Sudoku" << std::endl;
         std::cout << "2: Triangulo de pascal" << std::endl;
-<<<<<<< HEAD
         std::cout << "3: -" << std::endl;
         std::cout << "4: Cuadrado Latino" << std::endl;
         std::cout << "5: -" << std::endl;
-=======
-        std::cout << "3: Cuadrado magico" << std::endl;
-        std::cout << "4: Matriz identidad" << std::endl;
-        std::cout << "5: Cuadrado latino" << std::endl;
->>>>>>> 4d9124bb40aa3f3314e8aede14bf9e21864a4f2c
         std::cout << "6: salir" << std::endl;
         std::cout << std::endl;
 
@@ -72,7 +59,6 @@ void Application::run() {
             }
             break;
             case 4: {
-<<<<<<< HEAD
                 std::cout << "Ingrese el numero de la dimension del cuadrado latino " << std::endl;
                 int size;
                 std::cin >> size;
@@ -81,19 +67,6 @@ void Application::run() {
                 // MatrixUtils::generate_latin_square_origin(mat);
                 // MatrixUtils::print_matrix(mat);
                 
-=======
-                int n;
-
-                std::cout << "ingrese la dimension de la matriz: ";
-                std::cin >> n;
-
-                IdentityMatrix matrix(n);
-                matrix.print();
-
-                std::cout << std::endl << std::endl;
-                matrix.calculate();
-                matrix.print();
->>>>>>> 4d9124bb40aa3f3314e8aede14bf9e21864a4f2c
             }
             case 5: {
                 int size;
