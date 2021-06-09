9b0b1aa96f16eb32df24fbbf0fa2b08d3a27f910
sdiego616 <sdjimenez4@espe.edu.ec>
2021-06-08 22:32:26 -0500

    added latin square project

---
+++ workshops/06_04062021_matrix_projects/latin_square/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <limits>
+#include "matrix.h"
+#include "matrix_utils.h"
+
+int main(int argc, char **argv) {
+    // int size;
+    // int exponent;
+
+    // do {
+    //     std::cout << "ingrese la dimension de la matriz: ";
+    //     std::cin >> size;
+
+    //     if (size < 2 || std::cin.fail()) {
+    //         std::cout << std::endl << "[el valor debe ser superior a 2]" << std::endl;
+    //         std::cin.clear();
+    //         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //     }
+    // } while (size < 2);
+
+    // Matrix<long long> matrixA(size);
+    // Matrix<long long> matrixB(size);
+    // Matrix<long long> matrixC(size);
+    // Matrix<long long> matrixD(size);
+
+    // MatrixUtils::fill_random(matrixA, 0, 9);
+    // MatrixUtils::fill_random(matrixB, 0, 9);
+
+    // std::cout << std::endl << "Matriz A:" << std::endl;
+    // MatrixUtils::print_matrix(matrixA);
+
+    // std::cout << std::endl << "Matriz B:" << std::endl;
+    // MatrixUtils::print_matrix(matrixB);
+
+    // matrixC = MatrixUtils::multiply(matrixA, matrixB);
+
+    // std::cout << std::endl << "(A * B) :" << std::endl;
+    // MatrixUtils::print_matrix(matrixC);
+
+    // do {
+    //     std::cout << "ingrese un valor: ";
+    //     std::cin >> exponent;
+
+    //     if (size < 0 || std::cin.fail()) {
+    //         std::cout << std::endl << "[el valor debe ser un valor numerico positivo valido]" << std::endl;
+    //         std::cin.clear();
+    //         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //         size = -1;
+    //     }
+    // } while (size < 0);
+
+    // matrixD = MatrixUtils::pow(matrixC, exponent);
+
+    // std::cout << std::endl << "(A * B) ^ " << exponent << " :" << std::endl;
+    // MatrixUtils::print_matrix(matrixD);
+    int size = 6;
+    Matrix<long long> mat(size);
+    //MatrixUtils::generate_identity_random(mat);
+    MatrixUtils::generate_latin_square_origin(mat);
+    MatrixUtils::print_matrix(mat);
+    return 0;
+}
