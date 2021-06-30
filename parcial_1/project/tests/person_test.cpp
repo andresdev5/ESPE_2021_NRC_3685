74e261ba51e6e4d12fec26f16b51c5efd618a25a
Andres Jonathan <whitedev93@gmail.com>
2021-06-29 23:31:22 -0500

    fixed wkhtml binaries

--- parcial_1/project/tests/person_test.cpp
+++ parcial_1/project/tests/person_test.cpp
@@ -12,3 +12,14 @@ TEST(PersonTest, FieldsPerson) {
     EXPECT_EQ(person_a.id(), "1726744293");
     EXPECT_EQ(person_a.firstname(), "john doe");
 }
+
+TEST(PersonTest, PersonConstructor) {
+    Person person("1726744293", "John", "Doe", "Quito", "0998598514", "jhondoe@espe.edu.ec");
+
+    EXPECT_EQ(person.id(), "1726744293");
+    EXPECT_EQ(person.firstname(), "John");
+    EXPECT_EQ(person.lastname(), "Doe");
+    EXPECT_EQ(person.address(), "Quito");
+    EXPECT_EQ(person.phone(), "0998598514");
+    EXPECT_EQ(person.email(), "jhondoe@espe.edu.ec");
+}
