bbbf59f66fae21ab4f69281ef17941885c0f9b82
gpaguirre3 <gabrielaguirre1104@hotmail.com>
2021-06-30 00:02:38 -0500

    added unit tests

--- parcial_1/project/tests/person_test.cpp
+++ parcial_1/project/tests/person_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "../include/person.h"
 #include "../src/person.cpp"
+#include "../include/linked_list.h"
+#include "../include/utils.h"
 
 // check person attributes
 TEST(PersonTest, FieldsPerson) {
@@ -22,4 +24,22 @@ TEST(PersonTest, PersonConstructor) {
     EXPECT_EQ(person.address(), "Quito");
     EXPECT_EQ(person.phone(), "0998598514");
     EXPECT_EQ(person.email(), "jhondoe@espe.edu.ec");
+}
+
+TEST(PersonTest, UniqueEmailCheck) {
+    Person person_a("1726744293", "Jhon", "Doe", "Quito", "593998598514", "");
+    Person person_b("1727620054", "Jerry", "Doe", "Coshapamba", "593959660968", "");
+
+    LinkedList<Person> persons;
+
+    std::string email = generateUniqueEmail(persons, person_a, "espe.edu.ec");
+    person_a.email(email);
+    persons.push_back(person_a);
+
+    
+    email = generateUniqueEmail(persons, person_b, "espe.edu.ec");
+    person_b.email(email);
+    persons.push_back(person_b);
+
+    EXPECT_NE(person_a.email(), person_b.email());
 }
